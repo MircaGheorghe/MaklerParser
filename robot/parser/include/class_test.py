be5fd54cea3a5190b5bf9fbbe88a4794d1b7f7b2
gheorghe <giony.mirca@gmail.com>
2020-01-16 16:21:53 +0200

    add null_date link

--- robot/parser/include/class_test.py
+++ robot/parser/include/class_test.py
@@ -154,7 +154,6 @@ class pasteMakler:
         self.driver.get('https://makler.md')
         self.driver.get_screenshot_as_file('screenshots/getAfterPageScreen.png')
         print("a intrat pe site")
-
         self.driver.find_element_by_class_name('ui-icon-closethick').click()
         self.driver.implicitly_wait(3)
         print("a asteptat 3 secunde")
