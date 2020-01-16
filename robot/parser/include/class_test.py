55c20ca4b4ce1dc9e5eb1175b920030a18645c84
gheorghe <giony.mirca@gmail.com>
2020-01-16 12:41:06 +0200

    add null_date link

--- robot/parser/include/class_test.py
+++ robot/parser/include/class_test.py
@@ -142,7 +142,7 @@ class pasteMakler:
         try:
             chrome_options.add_argument('--headless')
             chrome_options.add_argument('--disable-gpu')
-            # chrome_options.add_argument('--no-sandbox')
+            chrome_options.add_argument('--no-sandbox')
             chrome_options.add_argument('--disable-dev-shm-usage')
             self.driver = webdriver.Chrome("/usr/bin/chromedriver", options=chrome_options)
         except:
