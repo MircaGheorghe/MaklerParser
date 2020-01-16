de11d0965cdf19095fd0fd47c2a4d42c68db863f
gheorghe <giony.mirca@gmail.com>
2020-01-16 17:18:48 +0200

    add null_date link

--- robot/parser/include/class_test.py
+++ robot/parser/include/class_test.py
@@ -154,9 +154,8 @@ class pasteMakler:
         self.driver.get('https://makler.md')
         self.driver.get_screenshot_as_file('screenshots/getAfterPageScreen.png')
         print("a intrat pe site")
-        # self.driver.implicitly_wait(2)
-        sleep(2)
-        print("a asteptat 2 secunde")
+        self.driver.implicitly_wait(3)
+        print("a asteptat 3 secunde")
 
         try:
             self.driver.find_element_by_class_name('ui-icon-closethick').click()
