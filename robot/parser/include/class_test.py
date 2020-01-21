14aa5e81cb7d4dfc0df684d567603ede50c8169e
gheorghe <giony.mirca@gmail.com>
2020-01-21 15:39:21 +0200

    imobiliare

--- robot/parser/include/class_test.py
+++ robot/parser/include/class_test.py
@@ -342,14 +342,14 @@ class pasteMakler:
             pass
 
 
-    # def paste_post(self, phone):
-    #     sleep(2)
-    #     self.driver.find_element_by_id(phone).click()
-    #     self.driver.find_element_by_class_name('saveBtn').click()
-    #     try:
-    #         self.driver.find_element_by_id('confirm_dialog').find_element_by_class_name('buttons').find_element_by_tag_name(a).click()
-    #     except:
-    #         self.driver.get_screenshot_as_file('screenshots/postare.png')
+    def paste_post(self, phone):
+        sleep(2)
+        self.driver.find_element_by_id(phone).click()
+        self.driver.find_element_by_class_name('saveBtn').click()
+        try:
+            self.driver.find_element_by_id('confirm_dialog').find_element_by_class_name('buttons').find_element_by_tag_name(a).click()
+        except:
+            self.driver.get_screenshot_as_file('screenshots/postare.png')
 
     def quit_driver(self):
         self.driver.quit()
